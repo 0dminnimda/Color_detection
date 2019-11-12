4ccc9887fd0a80f1dbe6a4b622b74d79f9739690
0dminnimda <0dminnimda@gmail.com>
2019-11-12 15:32:37 +0300

    Update 1alg.py

--- 1alg.py
+++ 1alg.py
@@ -61,6 +61,84 @@ def walk(mou, dirX, dirY, rangee, tim):
     time.sleep(tim-t/2)
     mou.release(Button.left)
 
+def closest(arr, X, Y, x0, y0):
+    r = 2
+    if arr == []:
+        img = np.zeros((Y, X, 3), dtype = "uint8")
+        cv.line(img, (0, int(y0)), (X, int(y0)), (255, 0, 0), 2) # hor
+        cv.line(img, (int(x0), 0), (int(x0), Y), (255, 0, 0), 2) # ver
+        #cv.line(img, (int( X/2 + X/(2*(1 + ma.sqrt(2)))*ma.sqrt(r) - X/2+x0 ), int(0 - Y/2+y0)), (int( X/2 - X/(2*(1+ma.sqrt(2)))*ma.sqrt(r) - X/2+x0 ), int(Y - Y/2+y0)), (255, 0, 0), 2)
+        #cv.line(img, (int( X/2 - X/(2*(1 + ma.sqrt(2)))*ma.sqrt(r) - X/2+x0 ), int(0 - Y/2+y0)), (int( X/2 + X/(2*(1+ma.sqrt(2)))*ma.sqrt(r) - X/2+x0 ), int(Y - Y/2+y0)), (255, 0, 0), 2)
+        cv.circle(img, (int(x0), int(y0)), 10, (0,0,255), 3)
+        cv.putText(img, "0", (int(x0)-10, int(y0)+40), cv.FONT_HERSHEY_SIMPLEX, 1, (0,0,255), 2)
+        cv.imshow("img", img)
+        return False, False
+    else:
+        img = np.zeros((Y, X, 3), dtype = "uint8")
+        m = arr[0]
+        c = 0
+        for j in arr:
+            c += 1
+            new = ma.sqrt(ma.fabs(j[0]-x0)**2 + ma.fabs(j[1]-y0)**2)
+            old = ma.sqrt(ma.fabs(m[0]-x0)**2 + ma.fabs(m[1]-y0)**2)
+            cv.circle(img, (int(j[0]), int(j[1])), 15, (255,0,0), 2)
+            cv.putText(img, "%d" % new, (int(j[0])+10,int(j[1])-10), cv.FONT_HERSHEY_SIMPLEX, 1, (0,255,255), 2)
+            if new < old:
+                m = j
+        cv.line(img, (0, int(y0)), (X, int(y0)), (255, 0, 0), 2) # hor
+        cv.line(img, (int(x0), 0), (int(x0), Y), (255, 0, 0), 2) # ver
+        #cv.line(img, (int( X/2 + X/(2*(1 + ma.sqrt(2)))*ma.sqrt(r) - X/2+x0 ), int(0 - Y/2+y0)), (int( X/2 - X/(2*(1+ma.sqrt(2)))*ma.sqrt(r) - X/2+x0 ), int(Y - Y/2+y0)), (255, 0, 0), 2)
+        #cv.line(img, (int( X/2 - X/(2*(1 + ma.sqrt(2)))*ma.sqrt(r) - X/2+x0 ), int(0 - Y/2+y0)), (int( X/2 + X/(2*(1+ma.sqrt(2)))*ma.sqrt(r) - X/2+x0 ), int(Y - Y/2+y0)), (255, 0, 0), 2)
+        cv.line(img, (int(x0), int(y0)), (int(m[0]), int(m[1])), (0, 0, 255), 2)
+        cv.circle(img, (int(m[0]), int(m[1])), 15, (0, 0, 255), -1)
+        cv.circle(img, (int(x0), int(y0)), 10, (255, 0, 0), 3)
+        cv.putText(img, "%d" % c, (int(x0)-10, int(y0)+40), cv.FONT_HERSHEY_SIMPLEX, 1, (0,0,255), 2)
+        cv.imshow("img", img)
+        m[0], m[1] = m[0]-x0, m[1]-y0
+        return m, old
+
+def order_points(pts):
+    #thanks for creating this function:
+    #https://www.pyimagesearch.com/2014/08/25/4-
+    #point-opencv-getperspective-transform-example/
+	rect = np.zeros((4, 2), dtype = "float32")
+
+	s = pts.sum(axis = 1)
+	rect[0] = pts[np.argmin(s)]
+	rect[2] = pts[np.argmax(s)]
+
+	diff = np.diff(pts, axis = 1)
+	rect[1] = pts[np.argmin(diff)]
+	rect[3] = pts[np.argmax(diff)]
+
+	return rect
+
+def persp_form(image, pts):
+    #thanks for creating this function:
+    #https://www.pyimagesearch.com/2014/08/25/4-
+    #point-opencv-getperspective-transform-example/
+	rect = order_points(pts)
+	(tl, tr, br, bl) = rect
+ 
+	widthA = np.sqrt(((br[0] - bl[0]) ** 2) + ((br[1] - bl[1]) ** 2))
+	widthB = np.sqrt(((tr[0] - tl[0]) ** 2) + ((tr[1] - tl[1]) ** 2))
+	maxWidth = max(int(widthA), int(widthB))
+
+	heightA = np.sqrt(((tr[0] - br[0]) ** 2) + ((tr[1] - br[1]) ** 2))
+	heightB = np.sqrt(((tl[0] - bl[0]) ** 2) + ((tl[1] - bl[1]) ** 2))
+	maxHeight = max(int(heightA), int(heightB))
+
+	dst = np.array([
+		[0, 0],
+		[maxWidth - 1, 0],
+		[maxWidth - 1, maxHeight - 1],
+		[0, maxHeight - 1]], dtype = "float32")
+
+	M = cv2.getPerspectiveTransform(rect, dst)
+	warped = cv2.warpPerspective(image, M, (maxWidth, maxHeight))
+
+	return warped
+
 def k_prel(lit, key, ti):
     key.press(lit)
     time.sleep(ti)
@@ -132,7 +210,7 @@ def w_call(qq):
     ke = k_c()
     while 1:
         if qq[0] != 0:
-            pass#walk_key(ke,*qq)
+            walk_key(ke,*qq)
         pass
 
 def s_call(qq):
@@ -161,42 +239,6 @@ def end(mou, an, bn):
     mou.position = (an+130, bn-680)
     mou.click(Button.left, 1)
 
-def closest(arr, X, Y, x0, y0):
-    r = 2
-    if arr == []:
-        img = np.zeros((Y, X, 3), dtype = "uint8")
-        cv.line(img, (0, int(y0)), (X, int(y0)), (255, 0, 0), 2) # hor
-        cv.line(img, (int(x0), 0), (int(x0), Y), (255, 0, 0), 2) # ver
-        #cv.line(img, (int( X/2 + X/(2*(1 + ma.sqrt(2)))*ma.sqrt(r) - X/2+x0 ), int(0 - Y/2+y0)), (int( X/2 - X/(2*(1+ma.sqrt(2)))*ma.sqrt(r) - X/2+x0 ), int(Y - Y/2+y0)), (255, 0, 0), 2)
-        #cv.line(img, (int( X/2 - X/(2*(1 + ma.sqrt(2)))*ma.sqrt(r) - X/2+x0 ), int(0 - Y/2+y0)), (int( X/2 + X/(2*(1+ma.sqrt(2)))*ma.sqrt(r) - X/2+x0 ), int(Y - Y/2+y0)), (255, 0, 0), 2)
-        cv.circle(img, (int(x0), int(y0)), 10, (0,0,255), 3)
-        cv.putText(img, "0", (int(x0)-10, int(y0)+40), cv.FONT_HERSHEY_SIMPLEX, 1, (0,0,255), 2)
-        cv.imshow("img", img)
-        return False, False
-    else:
-        img = np.zeros((Y, X, 3), dtype = "uint8")
-        m = arr[0]
-        c = 0
-        for j in arr:
-            c += 1
-            new = ma.sqrt(ma.fabs(j[0]-x0)**2 + ma.fabs(j[1]-y0)**2)
-            old = ma.sqrt(ma.fabs(m[0]-x0)**2 + ma.fabs(m[1]-y0)**2)
-            cv.circle(img, (int(j[0]), int(j[1])), 15, (255,0,0), 2)
-            cv.putText(img, "%d" % new, (int(j[0])+10,int(j[1])-10), cv.FONT_HERSHEY_SIMPLEX, 1, (0,255,255), 2)
-            if new < old:
-                m = j
-        cv.line(img, (0, int(y0)), (X, int(y0)), (255, 0, 0), 2) # hor
-        cv.line(img, (int(x0), 0), (int(x0), Y), (255, 0, 0), 2) # ver
-        #cv.line(img, (int( X/2 + X/(2*(1 + ma.sqrt(2)))*ma.sqrt(r) - X/2+x0 ), int(0 - Y/2+y0)), (int( X/2 - X/(2*(1+ma.sqrt(2)))*ma.sqrt(r) - X/2+x0 ), int(Y - Y/2+y0)), (255, 0, 0), 2)
-        #cv.line(img, (int( X/2 - X/(2*(1 + ma.sqrt(2)))*ma.sqrt(r) - X/2+x0 ), int(0 - Y/2+y0)), (int( X/2 + X/(2*(1+ma.sqrt(2)))*ma.sqrt(r) - X/2+x0 ), int(Y - Y/2+y0)), (255, 0, 0), 2)
-        cv.line(img, (int(x0), int(y0)), (int(m[0]), int(m[1])), (0, 0, 255), 2)
-        cv.circle(img, (int(m[0]), int(m[1])), 15, (0, 0, 255), -1)
-        cv.circle(img, (int(x0), int(y0)), 10, (255, 0, 0), 3)
-        cv.putText(img, "%d" % c, (int(x0)-10, int(y0)+40), cv.FONT_HERSHEY_SIMPLEX, 1, (0,0,255), 2)
-        cv.imshow("img", img)
-        m[0], m[1] = m[0]-x0, m[1]-y0
-        return m, old
-
 def gtbp(name):
     out_name = []
     for i in name:
@@ -224,7 +266,7 @@ def main_f():
         name5 = [i+"5" for i in name1]
         #name6 = [i+"6" for i in name1]
         #ctb(name6, [255, 255, 255, 255, 255, 255])
-        ctb(name5, [87, 133, 0, 90, 255, 255])
+        ctb(name5, [90, 100, 155, 95, 255, 255])
         ctb(name4, [28, 67, 64, 44, 255, 255])
         ctb(name3, [0, 255, 111, 0, 255, 255])
         ctb(name2, [0, 0, 0, 0, 0, 255])
@@ -239,14 +281,22 @@ def main_f():
         #cap_new.set(cv.CAP_PROP_FRAME_WIDTH , 1024)
         #cap_new.set(cv.CAP_PROP_FRAME_HEIGHT , 720)
 
-        imggg = cv.imread("Brawl Stars_Screenshot_2019.10.10_21.59.28.jpg")
+        n = gtbp(name1)
+        l_b, u_b = np.array([n[0], n[1], n[2]]), np.array([n[3], n[4], n[5]])
+        n = gtbp(name2)
+        l_b2, u_b2 = np.array([n[0], n[1], n[2]]), np.array([n[3], n[4], n[5]])
+        n = gtbp(name3)
+        l_b3, u_b3 = np.array([n[0], n[1], n[2]]), np.array([n[3], n[4], n[5]])
+        n = gtbp(name4)
+        l_b4, u_b4 = np.array([n[0], n[1], n[2]]), np.array([n[3], n[4], n[5]])
+        n = gtbp(name5)
+        l_b5, u_b5 = np.array([n[0], n[1], n[2]]), np.array([n[3], n[4], n[5]])
+
+        #imggg = cv.imread("Brawl Stars_Screenshot_2019.10.10_21.59.28.jpg")
         pass
 
-    qq = Array('d', [0,0,0]) #mp.Queue()
-    #print(*qq, "\n\n\n")
-    qq_2 = Array('d', [0,0,0]) #mp.Queue()
-    #qq_3 = mp.Queue()
-    #qq2 = mp.Queue()
+    qq = Array('d', [0,0,0])
+    qq_2 = Array('d', [0,0,0])
     pr = Process(target=w_call, args=(qq,), daemon=True)
     pr_2 = Process(target=w_call, args=(qq_2,), daemon=True)
     #pr_3 = Process(target=w_call, args=(qq_3,), daemon=True)
@@ -292,16 +342,9 @@ def main_f():
         #gray = cv.cvtColor(bet, cv.COLOR_BGR2GRAY)
 
         for _ in range(1):
-            n = gtbp(name1)
-            l_b, u_b = np.array([n[0], n[1], n[2]]), np.array([n[3], n[4], n[5]])
-            n = gtbp(name2)
-            l_b2, u_b2 = np.array([n[0], n[1], n[2]]), np.array([n[3], n[4], n[5]])
-            n = gtbp(name3)
-            l_b3, u_b3 = np.array([n[0], n[1], n[2]]), np.array([n[3], n[4], n[5]])
-            n = gtbp(name4)
-            l_b4, u_b4 = np.array([n[0], n[1], n[2]]), np.array([n[3], n[4], n[5]])
-            n = gtbp(name5)
-            l_b5, u_b5 = np.array([n[0], n[1], n[2]]), np.array([n[3], n[4], n[5]])
+            
+            #n = gtbp(name6)
+            #l_b6, u_b6 = np.array([n[0], n[1], n[2]]), np.array([n[3], n[4], n[5]])
 
             mask = cv.inRange(hsv, l_b, u_b) # charapters
             #res = cv.bitwise_and(frame, frame, mask=mask)
@@ -312,7 +355,9 @@ def main_f():
             mask4 = cv.inRange(hsv, l_b4, u_b4) # friends
             #res4 = cv.bitwise_and(frame, frame, mask=mask4)
             mask5 = cv.inRange(hsv, l_b5, u_b5)
-            res5 = cv.bitwise_and(frame, frame, mask=mask5)
+            #res5 = cv.bitwise_and(frame, frame, mask=mask5)
+            #mask6 = cv.inRange(hsv, l_b6, u_b6)
+            #res6 = cv.bitwise_and(frame, frame, mask=mask6)
 
             bet_mask = cv.bitwise_or(mask2, mask)
             bet_mask2 = cv.bitwise_or(bet_mask, mask3)
@@ -361,7 +406,7 @@ def main_f():
                 x = dM10 / dArea
                 y = dM01 / dArea
                 cv.circle(bet, (int(x), int(y)), 5, (0,0,0), -1)
-                #arr.append([x,y])
+                arr.append([x,y])
 
         for i in contour:
             moments = cv.moments(i, 1)
@@ -381,12 +426,12 @@ def main_f():
             pass#break
 
         c += 1
-        c2 += 1
+        #c2 += 1
         #c3 += 1
         if clo != False:
             if c % val == 0:
                 c -= val
-                qq[0], qq[1], qq[2] = clo[0], clo[1], t
+                #qq[0], qq[1], qq[2] = clo[0], clo[1], t
 
             #if c2 % val == 0:
             #    c2 -= val+1
@@ -399,7 +444,8 @@ def main_f():
         #cv.imshow("res2", res2)
         #cv.imshow("res3", res3)
         #cv.imshow("res4", res4)
-        cv.imshow("res5", res5)
+        #cv.imshow("res5", res5)
+        #cv.imshow("res6", res6)
         cv.imshow("bet", bet)
 
         #if cv.waitKey(3) & 0xFF == ord('4'):
@@ -422,5 +468,4 @@ def main_f():
     cap.release()
 
 if __name__ == '__main__':
-
     main_f()
